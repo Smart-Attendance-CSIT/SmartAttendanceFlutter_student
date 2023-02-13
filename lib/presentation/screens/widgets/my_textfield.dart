19049a2068edbc9319c81e581af8eff65b8a8805
sadikshya <ssadikshya01@gmail.com>
2023-02-13 22:09:31 +0545

    made button and text widget commented

--- lib/presentation/screens/widgets/my_textfield.dart
+++ lib/presentation/screens/widgets/my_textfield.dart
@@ -1,41 +1,41 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class MyTextField extends StatelessWidget {
-  final controller;
-  final String hintText;
-  final bool obscuredText;
-  final Icon prefixicon;
+// class MyTextField extends StatelessWidget {
+//   final controller;
+//   final String hintText;
+//   final bool obscuredText;
+//   final Icon prefixicon;
 
-  const MyTextField({
-    super.key,
-    required this.controller,
-    required this.hintText,
-    required this.obscuredText,
-    required this.prefixicon,
-  });
+//   const MyTextField({
+//     super.key,
+//     required this.controller,
+//     required this.hintText,
+//     required this.obscuredText,
+//     required this.prefixicon,
+//   });
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25.0),
-      child: TextField(
-        controller: controller,
-        obscureText: obscuredText,
-        decoration: InputDecoration(
-            prefixIcon: prefixicon,
-            enabledBorder: const OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.white),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.grey.shade400),
-            ),
-            fillColor: Colors.grey.shade200,
-            filled: true,
-            hintText: hintText,
-            hintStyle: TextStyle(
-              color: Colors.grey[500],
-            )),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.symmetric(horizontal: 25.0),
+//       child: TextField(
+//         controller: controller,
+//         obscureText: obscuredText,
+//         decoration: InputDecoration(
+//             prefixIcon: prefixicon,
+//             enabledBorder: const OutlineInputBorder(
+//               borderSide: BorderSide(color: Colors.white),
+//             ),
+//             focusedBorder: OutlineInputBorder(
+//               borderSide: BorderSide(color: Colors.grey.shade400),
+//             ),
+//             fillColor: Colors.grey.shade200,
+//             filled: true,
+//             hintText: hintText,
+//             hintStyle: TextStyle(
+//               color: Colors.grey[500],
+//             )),
+//       ),
+//     );
+//   }
+// }
