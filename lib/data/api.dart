f60ed40238830fc8ae6bcb475471f891661421d8
sadikshya <ssadikshya01@gmail.com>
2023-02-16 22:00:22 +0545

    api services added in data folder

---
+++ lib/data/api.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import 'package:smart_attendance_student/data/api_service.dart';
+
+class Api {
+  Dio dio = new Dio();
+
+  void getClient() async {
+    dio.options.baseUrl = Apiconstants.apiUrl;
+    dio.options.headers = {
+      HttpHeaders.acceptHeader: 'application/json',
+      HttpHeaders.contentTypeHeader: 'application/json',
+      // TODO: After implementation of token this lines of code will be uncomment.
+      // HttpHeaders.authorizationHeader:
+      //     "Bearer " + SpUtil.getString(Constants.authToken),
+    };
+
+    dio.interceptors.add(PrettyDioLogger(
+      requestHeader: true,
+      requestBody: true,
+      responseBody: true,
+      responseHeader: false,
+      compact: false,
+    ));
+  }
+}
---
+++ lib/models/login_params.dart
@@ -0,0 +1,18 @@
+class Loginparams {
+  String? email;
+  String? password;
+
+  Loginparams({this.email, this.password});
+
+  Loginparams.fromJson(Map<String, dynamic> json) {
+    email = json['email'];
+    password = json['password'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['email'] = this.email;
+    data['password'] = this.password;
+    return data;
+  }
+}
