860bb990fc583659730641925021b5a150dfb0e0
sadikshya <ssadikshya01@gmail.com>
2023-02-16 22:01:07 +0545

    abit ui is modified in class screen

--- lib/presentation/screens/class_screen.dart
+++ lib/presentation/screens/class_screen.dart
@@ -20,7 +20,7 @@ class _ClassScreenState extends State<ClassScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.purple,
+        backgroundColor: Colors.deepPurple,
         title: const Text('Class 1'),
         leading: IconButton(
           onPressed: (() {}),
@@ -29,23 +29,40 @@ class _ClassScreenState extends State<ClassScreen> {
           ),
         ),
       ),
-      body: Center(
-        child: Container(
-          child: Column(children: [
-            TableCalendar(
-              locale: 'en_us',
-              rowHeight: 50,
-              headerStyle: const HeaderStyle(
-                  formatButtonVisible: false, titleCentered: true),
-              availableGestures: AvailableGestures.all,
-              selectedDayPredicate: ((day) => isSameDay(day, today)),
-              focusedDay: today,
-              firstDay: DateTime.utc(2010, 10, 16),
-              lastDay: DateTime.utc(2030, 3, 13),
-              onDaySelected: _onDaySelected,
-            )
-          ]),
-        ),
+      body: Column(
+        children: [
+          Center(
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(children: [
+                Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(), color: Colors.white12),
+                  padding: EdgeInsets.all(20),
+                  child: TableCalendar(
+                    locale: 'en_us',
+                    rowHeight: 50,
+                    headerStyle: const HeaderStyle(
+                        formatButtonVisible: false, titleCentered: true),
+                    availableGestures: AvailableGestures.all,
+                    selectedDayPredicate: ((day) => isSameDay(day, today)),
+                    focusedDay: today,
+                    firstDay: DateTime.utc(2010, 10, 16),
+                    lastDay: DateTime.utc(2030, 3, 13),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  padding: EdgeInsets.all(40),
+                  height: 60,
+                  color: Colors.deepPurple,
+                )
+              ]),
+            ),
+          ),
+        ],
       ),
     );
   }
