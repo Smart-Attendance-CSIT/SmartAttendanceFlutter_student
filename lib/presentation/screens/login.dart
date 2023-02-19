d43582d727c4dacfec035e3b2e33106299a1469e
sadikshya <ssadikshya01@gmail.com>
2023-02-19 11:25:39 +0545

    login ui changed

--- lib/presentation/screens/login.dart
+++ lib/presentation/screens/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smart_attendance_student/data/api_interface.dart';
 import 'package:smart_attendance_student/models/login_params.dart';
 import 'package:smart_attendance_student/presentation/screens/widgets/my_button.dart';
@@ -62,6 +63,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
                   //userfield
                   TextFormField(
+                    controller: emailController,
                     validator: (value) {
                       if (value == null || value.isEmpty) {
                         return 'Please Enter Email';
@@ -74,10 +76,10 @@ class _LoginScreenState extends State<LoginScreen> {
                       prefixIcon: Icon(Icons.email),
                       hintText: 'Enter Email',
                       enabledBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white),
+                        borderSide: BorderSide(color: Colors.black),
                       ),
                       focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey.shade400),
+                        borderSide: BorderSide(color: Colors.white),
                       ),
                       fillColor: Colors.grey.shade200,
                       filled: true,
@@ -88,6 +90,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
                   //passwordfield
                   TextFormField(
+                    controller: passwordController,
                     validator: (value) {
                       if (value == null || value.isEmpty) {
                         return 'Please Enter Password';
@@ -119,20 +122,6 @@ class _LoginScreenState extends State<LoginScreen> {
 
                   const SizedBox(height: 10),
 
-                  //forget password?
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text(
-                          'Forget Password?',
-                          style: TextStyle(color: Colors.grey[600]),
-                        ),
-                      ],
-                    ),
-                  ),
-
                   const SizedBox(height: 10),
 
                   //sign in button
@@ -163,43 +152,43 @@ class _LoginScreenState extends State<LoginScreen> {
 
                   const SizedBox(height: 50),
 
-                  //or continue with
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Divider(
-                            thickness: 0.5,
-                            color: Colors.grey.shade400,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                          child: Text(
-                            'Or continue with',
-                            style: TextStyle(color: Colors.grey.shade700),
-                          ),
-                        ),
-                        Expanded(
-                          child: Divider(
-                            thickness: 0.5,
-                            color: Colors.grey.shade400,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+                  // //or continue with
+                  // Padding(
+                  //   padding: const EdgeInsets.symmetric(horizontal: 25),
+                  //   child: Row(
+                  //     children: [
+                  //       Expanded(
+                  //         child: Divider(
+                  //           thickness: 0.5,
+                  //           color: Colors.grey.shade400,
+                  //         ),
+                  //       ),
+                  //       Padding(
+                  //         padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  //         child: Text(
+                  //           'Or continue with',
+                  //           style: TextStyle(color: Colors.grey.shade700),
+                  //         ),
+                  //       ),
+                  //       Expanded(
+                  //         child: Divider(
+                  //           thickness: 0.5,
+                  //           color: Colors.grey.shade400,
+                  //         ),
+                  //       ),
+                  //     ],
+                  //   ),
+                  // ),
 
-                  SizedBox(height: 50),
+                  // SizedBox(height: 50),
 
-                  //google
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      SquareTile(imagePath: 'assets/images/google.png'),
-                    ],
-                  ),
+                  // //google
+                  // Row(
+                  //   mainAxisAlignment: MainAxisAlignment.center,
+                  //   children: const [
+                  //     SquareTile(imagePath: 'assets/images/google.png'),
+                  //   ],
+                  // ),
                 ]),
               ),
             ),
@@ -211,24 +200,18 @@ class _LoginScreenState extends State<LoginScreen> {
 
   void login(Loginparams loginparams) {
     ApiInterface apiInterface = ApiInterface();
-    apiInterface.login(loginparams).then((res) {
-      if (res.status == 200) {
-        var data = res.body;
-        print(data);
-        if (data.length > 0) {
-          final StudentResponsemodel studentResponsemodel =
-              StudentResponsemodel.fromJson(data);
-          // StudentResponsemodel studentResponsemodel =
-          //     data.map<StudentResponsemodel>(
-          //         (json) => StudentResponsemodel.fromJson(json));
-          print(studentResponsemodel.user!.firstName);
-          jumpToHomeScreen(context);
-          ScaffoldMessenger.of(context)
-              .showSnackBar(SnackBar(content: Text("Login Sucessfully")));
-        }
+    apiInterface.login(loginparams).then((res) async {
+      StudentResponsemodel studentResponsemodel =
+          StudentResponsemodel.fromJson(res.body);
+      print(studentResponsemodel);
+      if (studentResponsemodel.success == true) {
+        SharedPreferences sharedPreferences =
+            await SharedPreferences.getInstance();
+        sharedPreferences.setString("user", studentResponsemodel.user!.id!);
+        sharedPreferences.setString("key", studentResponsemodel.token!);
       } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text("Email or Password incorrect")));
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text(studentResponsemodel.msg!)));
       }
     });
   }
--- lib/presentation/screens/splash_screen.dart
+++ lib/presentation/screens/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:lottie/lottie.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smart_attendance_student/config/constants/assets_constants.dart';
 
 import '../../config/constants/navigation/app_navigation.dart';
@@ -15,7 +16,6 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen>
     with SingleTickerProviderStateMixin {
   late AnimationController lottieController;
-  bool isAnimationComplete = false;
 
   @override
   void initState() {
@@ -25,11 +25,14 @@ class _SplashScreenState extends State<SplashScreen>
       vsync: this,
       duration: const Duration(seconds: 4),
     );
-    lottieController.addStatusListener((status) {
+    lottieController.addStatusListener((status) async {
       if (status == AnimationStatus.completed) {
-        setState(() {
-          isAnimationComplete = true;
-        });
+        final prefs = await SharedPreferences.getInstance();
+        if (prefs.getString('user') == "" || prefs.getString('user') == null) {
+          jumpToLoginScreen(context);
+        } else {
+          jumpToHomeScreen(context);
+        }
       }
     });
   }
@@ -46,52 +49,46 @@ class _SplashScreenState extends State<SplashScreen>
       body: Container(
           height: double.infinity,
           width: double.infinity,
-          child: !isAnimationComplete
-              ? Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Text(
-                          "Smart Attendance",
-                        ),
-                      ],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    "Smart Attendance",
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 52,
+              ),
+              Lottie.asset(splashAnimation,
+                  height: 300,
+                  width: 300,
+                  fit: BoxFit.cover,
+                  controller: lottieController, onLoaded: (composition) {
+                lottieController.forward();
+              }),
+              const SizedBox(
+                height: 100,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  SizedBox(
+                    height: 70,
+                    width: 70,
+                    child: Icon(
+                      Icons.qr_code,
+                      color: Colors.white,
                     ),
-                    const SizedBox(
-                      height: 52,
-                    ),
-                    Lottie.asset(splashAnimation,
-                        height: 300,
-                        width: 300,
-                        fit: BoxFit.cover,
-                        controller: lottieController, onLoaded: (composition) {
-                      lottieController.forward();
-                    }),
-                    const SizedBox(
-                      height: 100,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        SizedBox(
-                          height: 70,
-                          width: 70,
-                          child: Icon(
-                            Icons.qr_code,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                )
-              : TextButton(
-                  onPressed: () {
-                    jumpToLoginScreen(context);
-                  },
-                  child: const Text("Go to login screen"))),
+                  ),
+                ],
+              ),
+            ],
+          )),
     );
   }
 }
