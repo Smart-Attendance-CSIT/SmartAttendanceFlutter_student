4c9457f9f5c3359977c3ae40e383b1eaa96b737a
sadikshya <ssadikshya01@gmail.com>
2023-02-19 11:23:49 +0545

    added dio

--- lib/data/api_service.dart
+++ lib/data/api_service.dart
@@ -1,5 +1,8 @@
 class Apiconstants {
-  static const String apiUrl =
-      "https://635ccbb9-830b-46f0-824d-1d9a76c0e561.mock.pstmn.io";
+  // static const String apiUrl =
+  //     "https://635ccbb9-830b-46f0-824d-1d9a76c0e561.mock.pstmn.io";
+  // static const String apiUrl = "http://localhost:3000";
+  // for emulator
+  static const String apiUrl = "http://10.0.2.2:3000";
   static const String loginUrl = "/auth/login";
 }
