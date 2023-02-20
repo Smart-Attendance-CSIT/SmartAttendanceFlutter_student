004d85ef603c1d6cd839648b530a5c685cabfba9
sadikshya <ssadikshya01@gmail.com>
2023-02-21 01:25:33 +0545

    added logout button

--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/basic.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smart_attendance_student/main.dart';
 import 'package:smart_attendance_student/config/constants/navigation/app_navigation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import '../../config/constants/app_theme.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -29,49 +31,63 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        backgroundColor: Colors.deepPurple,
-        title: Text(
+        backgroundColor: appColor1,
+        title: const Text(
           "Classes",
           style: TextStyle(
-              color: Colors.black87,
-              fontWeight: FontWeight.bold,
-              fontSize: 22.0),
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 22.0),
         ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextButton(
+              onPressed: () async {
+                SharedPreferences sharedPreferences =
+                    await SharedPreferences.getInstance();
+                sharedPreferences.setString("user", "");
+                sharedPreferences.setString("key", "");
+                jumpToLoginScreen(context);
+              },
+              child: const Text(
+                "Logout",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          )
+        ],
       ),
       body: Center(
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
           Expanded(
-            child: Container(
-              // color: Colors.blueGrey[500],
-              child: ListView.builder(
-                  itemCount: name.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      padding: EdgeInsets.all(15),
-                      child: (Card(
-                        elevation: 50,
-                        shadowColor: Colors.black,
-                        color: Colors.white,
-                        child: ListTile(
-                          onTap: (() => jumpToOptionScreen(context)),
-                          title: Text(
-                            name[index].toString(),
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          leading: Icon(
-                            Icons.menu_book,
+            child: ListView.builder(
+                itemCount: name.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 10),
+                    child: (Card(
+                      elevation: 10,
+                      shadowColor: Colors.black,
+                      color: appColor4,
+                      child: ListTile(
+                        onTap: (() => jumpToOptionScreen(context)),
+                        title: Text(
+                          name[index].toString(),
+                          style: const TextStyle(
                             color: Colors.black,
-                            size: 27,
+                            fontSize: 20,
+                            // fontWeight: FontWeight.bold,
                           ),
                         ),
-                      )),
-                    );
-                  }),
-            ),
+                        leading: const Icon(
+                          Icons.menu_book,
+                          color: Colors.black,
+                          size: 27,
+                        ),
+                      ),
+                    )),
+                  );
+                }),
           ),
           // SizedBox(
           //   height: 100,
