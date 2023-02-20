ac5836111f9f1fc535d2064d62c2905f96498e99
sadikshya <ssadikshya01@gmail.com>
2023-02-21 01:10:19 +0545

    added theme color

---
+++ lib/config/constants/app_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const primaryAppColor = Colors.white;
+const appColor1 = Color(0xff181823);
+const appColor2 = Color(0xff537FE7);
+const appColor3 = Color(0xffC0EEF2);
+const appColor4 = Color(0xffE9F8F9);
