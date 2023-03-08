05b64c8ad5653660575f44fcce50ac9a0fd3dafc
sadikshya <ssadikshya01@gmail.com>
2023-03-08 12:59:18 +0545

    circular indiator added

--- lib/presentation/screens/leave.dart
+++ lib/presentation/screens/leave.dart
@@ -18,6 +18,7 @@ class _LeaveScreenState extends State<LeaveScreen> {
   final dateController = TextEditingController();
 
   final reasonController = TextEditingController();
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -27,121 +28,128 @@ class _LeaveScreenState extends State<LeaveScreen> {
         title: Text("Leave Request"),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            child: Center(
-              child: Form(
-                key: _formKey,
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(height: 10),
+      body: !isLoading
+          ? SingleChildScrollView(
+              child: SafeArea(
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  child: Center(
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(height: 10),
 
-                      const Text(
-                        "Select Date:  ",
-                        style: labelStyle,
-                        textAlign: TextAlign.left,
-                      ),
-
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: TextFormField(
-                          onTap: () async {
-                            FocusScope.of(context)
-                                .requestFocus(new FocusNode());
-
-                            DateTime? date = await showDatePicker(
-                                context: context,
-                                initialDate: DateTime.now().toUtc(),
-                                firstDate: DateTime(1900).toUtc(),
-                                lastDate: DateTime(2100).toUtc());
-                            DateTime utcDatetime = date!.toUtc();
-
-                            dateController.text = utcDatetime.toIso8601String();
-                          },
-                          controller: dateController,
-                          decoration: const InputDecoration(
-                            hintText: 'Select Date',
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black),
+                            const Text(
+                              "Select Date:  ",
+                              style: labelStyle,
+                              textAlign: TextAlign.left,
                             ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white),
+
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextFormField(
+                                onTap: () async {
+                                  FocusScope.of(context)
+                                      .requestFocus(new FocusNode());
+
+                                  DateTime? date = await showDatePicker(
+                                      context: context,
+                                      initialDate: DateTime.now().toUtc(),
+                                      firstDate: DateTime(1900).toUtc(),
+                                      lastDate: DateTime(2100).toUtc());
+                                  DateTime utcDatetime = date!.toUtc();
+
+                                  dateController.text =
+                                      utcDatetime.toIso8601String();
+                                },
+                                controller: dateController,
+                                decoration: const InputDecoration(
+                                  hintText: 'Select Date',
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.black),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.white),
+                                  ),
+                                  filled: true,
+                                ),
+                              ),
                             ),
-                            filled: true,
-                          ),
-                        ),
-                      ),
-                      const Text(
-                        "Write leave request message: ",
-                        style: labelStyle,
-                        textAlign: TextAlign.left,
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-
-                      //userfield
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: TextFormField(
-                          minLines: 3,
-                          maxLines: 4,
-                          controller: reasonController,
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please write valid reason';
-                            }
-                          },
-                          decoration: const InputDecoration(
-                            hintText: 'Write Reason',
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black),
+                            const Text(
+                              "Write leave request message: ",
+                              style: labelStyle,
+                              textAlign: TextAlign.left,
                             ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white),
+                            SizedBox(
+                              height: 5,
                             ),
-                            filled: true,
-                          ),
-                        ),
-                      ),
 
-                      SizedBox(
-                        height: 50,
-                        width: 500,
-                        child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor: appColor1),
-                            onPressed: () async {
-                              if (_formKey.currentState!.validate()) {
-                                String uploadMessage = await requestLeave(
-                                    dateController.text,
-                                    reasonController.text,
-                                    widget.groupCode);
+                            //userfield
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextFormField(
+                                minLines: 3,
+                                maxLines: 4,
+                                controller: reasonController,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please write valid reason';
+                                  }
+                                },
+                                decoration: const InputDecoration(
+                                  hintText: 'Write Reason',
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.black),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.white),
+                                  ),
+                                  filled: true,
+                                ),
+                              ),
+                            ),
 
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                    SnackBar(content: Text(uploadMessage)));
-                                // jumpToOptionScreen(context);
-                              }
-                            },
-                            child: const Text(
-                              'Submit',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold),
-                            )),
-                      ),
+                            SizedBox(
+                              height: 50,
+                              width: 500,
+                              child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                      backgroundColor: appColor1),
+                                  onPressed: () async {
+                                    if (_formKey.currentState!.validate()) {
+                                      String uploadMessage = await requestLeave(
+                                          dateController.text,
+                                          reasonController.text,
+                                          widget.groupCode);
 
-                      const SizedBox(height: 50),
-                    ]),
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(SnackBar(
+                                              content: Text(uploadMessage)));
+                                      Navigator.pop(context);
+                                      // jumpToOptionScreen(context);
+                                    }
+                                  },
+                                  child: const Text(
+                                    'Submit',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold),
+                                  )),
+                            ),
+
+                            const SizedBox(height: 50),
+                          ]),
+                    ),
+                  ),
+                ),
               ),
+            )
+          : const Center(
+              child: CircularProgressIndicator(),
             ),
-          ),
-        ),
-      ),
     );
   }
 
@@ -149,6 +157,9 @@ class _LeaveScreenState extends State<LeaveScreen> {
       String dateTime, String message, String groupCode) async {
     String uploadStatus = "";
     ApiInterface apiInterface = ApiInterface();
+    setState(() {
+      isLoading = true;
+    });
     try {
       uploadStatus = await apiInterface.leaveRequest(
           date: dateTime, message: message, groupId: groupCode);
